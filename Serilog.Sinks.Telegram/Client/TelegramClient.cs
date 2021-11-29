a1da4e44a9c31d8e2be99b444756c865178d7d74
Hans Gregersen Jensen <hgj@firetraders.dk>
2021-11-29 08:33:36 +0100

    ns

--- Serilog.Sinks.Telegram/Client/TelegramClient.cs
+++ Serilog.Sinks.Telegram/Client/TelegramClient.cs
@@ -4,7 +4,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
-namespace Serilog.Sinks.Telegram
+namespace Serilog.Sinks.Telegram.Client
 {
     public class TelegramClient
     {
